cab80e577f38211dacba44f89a284ee5d57a77c6
Christopher Patrick <cpatrick@nabancard.com>
2019-03-13 12:35:02 -0400

    Displaying video player on screen

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -6,10 +6,17 @@ const VideoDetail = ({ video }) => {
     if(!video) {
         return <div>Loading...</div>;
     }
+
+    const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
+
     return ( 
     <div>
+        <div className="ui embed">
+        <iframe src={videoSrc}  />
+        </div> 
         <div className="ui segment">
-        {video.snippet.title}
+        <h4 className="ui header">{video.snippet.title}</h4>
+        {video.snippet.description}
         </div>
     </div>
     );
